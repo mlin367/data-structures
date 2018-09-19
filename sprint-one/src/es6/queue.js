31392bd1dd5309f9ab9ef18c3d744eb40fb70c4a
Matthew Lin <mlin367@yahoo.com>
2018-09-19 16:14:18 -0700

    Implement queue es6 style

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.queueSize = 0;
+    this.queueExit = 0;
+  }
+
+  enqueue(value) {
+    this[this.queueSize] = value;
+    this.queueSize++;
+  }
+
+  dequeue() {
+    let temp = this[this.queueExit];
+    delete this[this.queueExit];
+    this.queueExit++;
+    return temp;
+  }
+
+  size() {
+    let diff = this.queueSize - this.queueExit;
+    if (diff < 0) {
+      return 0;
+    } else {
+      return diff;
+    }
   }
 
 }
