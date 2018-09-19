93b3856385eeed2700e1177fc236747684b481e5
Matthew Lin <mlin367@yahoo.com>
2018-09-19 16:13:33 -0700

    Implement stack es6 style

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.stackSize = 0;
+  }
+
+  push(value) {
+    this[this.stackSize] = value;
+    this.stackSize++;
+  }
+
+  pop(value) {
+    this.stackSize && this.stackSize--;
+    let temp = this[this.stackSize];
+    delete this[this.stackSize];
+    return temp;
+  }
+
+  size() {
+    return this.stackSize;
   }
 
 }
