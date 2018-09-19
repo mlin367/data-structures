1b49b068cebb106c1b852d5b570a038b98073560
Matthew Lin <mlin367@yahoo.com>
2018-09-19 14:56:27 -0700

    Implement stack functional-shared style

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var obj = {stackSize: 0};
+
+  extend(obj, stackMethods);
+
+  return obj;
 };
 
-var stackMethods = {};
+var extend = function(obj, methods){
+  for(let key in methods){
+    obj[key] = methods[key];
+  }
+};
 
+var stackMethods = {
 
+  push: function(value){
+    this[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop: function(){
+    this.stackSize && this.stackSize--;
+    let temp = this[this.stackSize];
+    delete this[this.stackSize];
+    return temp;
+  },
+
+  size: function(){
+    return this.stackSize;
+  }
+};
