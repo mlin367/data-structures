512ea7a848fa68a6ceb4dbb215b1dcf14d5425ab
Matthew Lin <mlin367@yahoo.com>
2018-09-21 15:47:22 -0700

    Implement hash table data structure

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,56 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let tuple = [k, v];
+  let bucket = this._storage.get(index);
+  if (bucket) {
+    this._storage.each((buck, i, storage) => {
+      if (i === index) {
+        storage[i].push(tuple);
+      }
+    });
+  } else {
+    this._storage.set(index, [tuple]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  let result = undefined;
+  if (bucket) {
+    this._storage.each((buck, i, storage) => {
+      if (i === index) {
+        buck.forEach(tuple => {
+          if (tuple[0] === k) {
+            result = tuple[1];
+          }
+        });
+      }
+    });
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  if (bucket) {
+    this._storage.each((buck, i, storage) => {
+      if (i === index) {
+        buck.forEach((tuple, index) => {
+          if (tuple[0] === k) {
+            delete storage[i][index];
+          }
+        });
+      }
+    });
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
  */
-
-
