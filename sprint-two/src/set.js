05a7cf502f57e155a84d9180d840020f41fdf298
Matthew Lin <mlin367@yahoo.com>
2018-09-20 17:34:46 -0700

    Implement the set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  if(this._storage.indexOf(item) !== -1) {
+    this._storage.splice(this._storage.indexOf(item), 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add is O(n) since it traverses the storage
+ contains is O(n) since it traverses the storage
+ remove is O(n) since it shifts elements in an array
  */
