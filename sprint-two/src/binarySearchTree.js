144df207aef300f2c0e3fd415ced4b87446f12e7
Matthew Lin <mlin367@yahoo.com>
2018-09-21 17:58:46 -0700

    Implement binarySearchTree data structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
 var BinarySearchTree = function(value) {
+
+  let bst = Object.create(bTreeMethods);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+
+  return bst;
 };
 
+var bTreeMethods = {
 
+  insert: function(value) {
+    let tree = Object.create(bTreeMethods);
+    tree.value = value;
+    tree.left = null;
+    tree.right = null;
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = tree;
+      } else {
+        this.insert.call(this.left, value);
+      }
+    } else {
+      if (!this.right) {
+        this.right = tree;
+      } else {
+        this.insert.call(this.right, value);
+      }
+    }
+  },
+
+  contains: function(value) {
+    if (this.value === value) {
+      return true;
+    } else if (value < this.value && this.left) {
+      return this.contains.call(this.left, value);
+    } else if (value > this.value && this.right) {
+      return this.contains.call(this.right, value);
+    } else {
+      return false;
+    }
+  },
+
+  depthFirstLog: function(cb) {
+    cb(this.value);
+    if (this.left) {
+      this.depthFirstLog.call(this.left, cb);
+    }
+    if (this.right) {
+      this.depthFirstLog.call(this.right, cb);
+    }
+  }
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
